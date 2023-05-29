4a234d289a41c9bb796b5d4b179e4176c045727b
HanyMagdy25 <hany.magdy25@gmail.com>
2023-05-29 13:22:44 +0300

    finish Navbar Responive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import {
   GithubIcon,
   LinkedInIcon,
@@ -28,62 +28,185 @@ const CustomLink = ({ href, title, className = "" }) => {
     </Link>
   );
 };
+const CustomMobileLink = ({ href, title, className = "", toggle }) => {
+  const router = useRouter();
+  const handleClick = () => {
+    toggle();
+    router.push(href);
+  };
+
+  return (
+    <button
+      href={href}
+      className={`${className} relative group text-light dark:text-dark my-2`}
+      onClick={handleClick}
+    >
+      {title}
+
+      <span
+        className={`inline-block h-[1px] bg-light absolute left-0 -bottom-0.5 
+              group-hover:w-full transition-[width] ease duration-300 ${
+                router.asPath === href ? "w-full" : "w-0"
+              } dark:bg-dark`}
+      ></span>
+    </button>
+  );
+};
 
 const Navbar = () => {
   const [mode, setMode] = useThemeSwitcher();
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <header
-      className="w-full px-32 py-8 font-medium flex items-center justify-between
-    dark:text-light"
+      className="w-full px-32 py-9 font-medium flex items-center justify-between
+    dark:text-light relative"
     >
-      <nav>
-        <CustomLink href="/" title="Home" className="mr-4" />
-        <CustomLink href="/about" title="About" className="mx-4" />
-        <CustomLink href="/projects" title="Projects" className="mx-4" />
-      </nav>
-      <Logo />
-      <nav className="flex items-center justify-center flex-wrap">
-        <motion.a
-          href="https://twitter.com/hanymagdyjr"
-          target={"_blank"}
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mx-3"
-        >
-          <TwitterIcon />
-        </motion.a>
-        <motion.a
-          href="https://github.com/HanyMagdy25"
-          target={"_blank"}
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mx-3"
-        >
-          <GithubIcon />
-        </motion.a>
-        <motion.a
-          href="https://www.linkedin.com/in/hany-magdy-7a0ab4175/"
-          target={"_blank"}
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mx-3"
-        >
-          <LinkedInIcon />
-        </motion.a>
+      <button
+        className="hidden lg:flex flex-col justify-center items-center"
+        onClick={handleClick}
+      >
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+            isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+          } `}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
+            isOpen ? "opacity-0" : "opacity-100"
+          } `}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+            isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
+          } `}
+        ></span>
+      </button>
 
+      <div className="w-full flex justify-between items-center lg:hidden">
+        <nav>
+          <CustomLink href="/" title="Home" className="mr-4" />
+          <CustomLink href="/about" title="About" className="mx-4" />
+          <CustomLink href="/projects" title="Projects" className="mx-4" />
+        </nav>
+        <nav className="flex items-center justify-center flex-wrap">
+          <motion.a
+            href="https://twitter.com/hanymagdyjr"
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+          >
+            <TwitterIcon />
+          </motion.a>
+          <motion.a
+            href="https://github.com/HanyMagdy25"
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+          >
+            <GithubIcon />
+          </motion.a>
+          <motion.a
+            href="https://www.linkedin.com/in/hany-magdy-7a0ab4175/"
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+          >
+            <LinkedInIcon />
+          </motion.a>
 
-        <button
-          onClick={() => setMode(mode === "light" ? "dark" : "light")}
-          className={`ml-3 flex items-center justify-center rounded-full p-1 
+          <button
+            onClick={() => setMode(mode === "light" ? "dark" : "light")}
+            className={`ml-3 flex items-center justify-center rounded-full p-1 
           ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`}
+          >
+            {mode === "dark" ? (
+              <SunIcon className={"fill-dark"} />
+            ) : (
+              <MoonIcon className={"fill-dark"} />
+            )}
+          </button>
+        </nav>
+      </div>
+
+      {isOpen ? (
+        <motion.div
+          initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
+          animate={{ scale: 1, opacity: 1 }}
+          className="min-w-[70vw] flex flex-col z-30 justify-between items-center
+       fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90
+        dark:bg-light/75 rounded-lg backdrop-blur-md py-32"
         >
-          {mode === "dark" ? (
-            <SunIcon className={"fill-dark"} />
-          ) : (
-            <MoonIcon className={"fill-dark"} />
-          )}
-        </button>
-      </nav>
+          <nav className="flex items-center flex-col justify-center">
+            <CustomMobileLink
+              href="/"
+              title="Home"
+              className=""
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href="/about"
+              title="About"
+              className=""
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href="/projects"
+              title="Projects"
+              className=""
+              toggle={handleClick}
+            />
+          </nav>
+          <nav className="flex items-center justify-center flex-wrap mt-2">
+            <motion.a
+              href="https://twitter.com/hanymagdyjr"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3 sm:mx-1"
+            >
+              <TwitterIcon />
+            </motion.a>
+            <motion.a
+              href="https://github.com/HanyMagdy25"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3 bg-light rounded-full dark:bg-dark sm:mx-1"
+            >
+              <GithubIcon />
+            </motion.a>
+            <motion.a
+              href="https://www.linkedin.com/in/hany-magdy-7a0ab4175/"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3 sm:mx-1"
+            >
+              <LinkedInIcon />
+            </motion.a>
+
+            <button
+              onClick={() => setMode(mode === "light" ? "dark" : "light")}
+              className={`ml-3 flex items-center justify-center rounded-full p-1 
+          ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`}
+            >
+              {mode === "dark" ? (
+                <SunIcon className={"fill-dark"} />
+              ) : (
+                <MoonIcon className={"fill-dark"} />
+              )}
+            </button>
+          </nav>
+        </motion.div>
+      ) : null}
+
+      <Logo />
     </header>
   );
 };
