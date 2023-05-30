7e85be61cc789156c46fe0c68876e7a362908c1e
HanyMagdy25 <hany.magdy25@gmail.com>
2023-05-30 19:39:22 +0300

    finish home Responive

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -2,15 +2,16 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import hireSvg from "../../assets/Senior_Frontend_Developer.svg";
-import { CircularText } from "./Icons";
+// import { CircularText } from "./Icons";
 
 const HireMe = () => {
   return (
     <div
       className="fixed left-4 bottom-4
-    flex items-center justify-center overflow-hidden"
+    flex items-center justify-center overflow-hidden
+     md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute"
     >
-      <div className="w-48 h-auto flex items-center justify-center relative">
+      <div className="w-48 h-auto flex items-center justify-center relative md:w-24">
         <Image
           src={hireSvg}
           alt="hireSvg"
@@ -24,7 +25,8 @@ const HireMe = () => {
           border-solid border-dark w-24 h-24 rounded-full font-semibold
            border-2 hover:text-dark hover:bg-light
            dark:bg-light dark:text-dark hover:dark:bg-dark 
-           hover:dark:text-light hover:dark:border-light"
+           hover:dark:text-light hover:dark:border-light
+           md:w-12 md:h-12 md:text-[10px]"
         >
           Hire Me
         </Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,20 +16,22 @@ export default function Home() {
         <meta name="description" content="Hany Magdy - Frontend Developer" />
       </Head>
       <main className="flex items-center text-dark w-full min-h-screen dark:text-light">
-        <Layout className="pt-0">
-          <div className="flex justify-center items-center w-full">
-            <div className="w-1/2">
+        <Layout className="pt-0 md:pt-16 sm:pt-8">
+          <div className="flex justify-center items-center w-full lg:flex-col">
+            <div className="w-1/2 md:w-full">
               <Image
                 src={profileImg}
+                priority
                 alt="HanyMagdy"
-                className="w-full h-auto"
+                className="w-full h-auto lg:hidden md:inline-block md:w-full"
               />
             </div>
-            <div className="w-1/2 flex flex-col items-center self-center">
+            <div className="w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center">
               {/* <h1>Turning Vision Into Reality With Code And Design. </h1> */}
               <AnimatedText
                 text="Turning Vision Into Reality With Code And Design."
-                className="!text-6xl !text-left"
+                className="!text-6xl !text-left xl:!text-5xl lg:!text-center 
+                lg:!text-6xl md:!text-5xl sm:!text-3xl"
               />
               <p className="my-4 text-base font-medium">
                 As a skilled full-stack developer, I am dedicated to turning
@@ -37,7 +39,7 @@ export default function Home() {
                 projects and articles, showcasing my expertise in React.js and
                 web development.
               </p>
-              <div className="flex items-center self-start mt-2">
+              <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
                   href="/Hany-Magdy-Frontend-ReactJs.pdf"
                   target={"_blank"}
@@ -45,7 +47,7 @@ export default function Home() {
                   text-lg font-semibold hover:bg-light hover:text-dark border-2
                   border-solid border-transparent hover:border-dark transition
                   duration-300 dark:bg-light dark:text-dark hover:dark:bg-dark 
-                hover:dark:text-light hover:dark:border-light"
+                hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base"
                   download={true}
                 >
                   Resume <LinkArrow className={"w-6 ml-1"} />
@@ -54,7 +56,7 @@ export default function Home() {
                   href="mailto:hany.magdy25@gmail.com"
                   target={"_blank"}
                   className="ml-4 text-lg font-medium items-center
-                   text-dark dark:text-light "
+                   text-dark dark:text-light underline md:text-base"
                 >
                   Contact
                 </Link>
@@ -63,7 +65,7 @@ export default function Home() {
           </div>
         </Layout>
         <HireMe />
-        <div className="absolute right-8 bottom-8 inline-block w-24">
+        <div className="absolute right-8 bottom-8 inline-block w-24 md:hidden">
           <Image src={lightBulb} alt="HanyMagdy" className="w-full h-auto" />
         </div>
       </main>
