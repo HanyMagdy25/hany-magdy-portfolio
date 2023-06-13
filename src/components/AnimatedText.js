48e2484d9a577ba9048ba0c3372afbbdcd310978
HanyMagdy25 <hany.magdy25@gmail.com>
2023-06-14 01:50:27 +0300

    animate

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -28,14 +28,18 @@ const singleWord = {
 
 const AnimatedText = ({ text, className = "" }) => {
   return (
-    <div className="w-full mx-auto py-2 flex items-center justify-center 
-    text-center overflow-hidden sm:py-0  ">
+    <div
+      className="w-full mx-auto py-2 flex items-center justify-center 
+    text-center overflow-hidden sm:py-0  "
+    >
       <motion.h1
         className={`inline-block w-full text-dark font-bold capitalize text-8xl
         dark:text-light ${className}`}
         variants={qoute}
         initial="initial"
         animate="animate"
+        whileInView={{ opacity: [0, 1], y: [40, 0] }}
+        transition={{ duration: 1.2 }}
       >
         {text?.split(" ").map((word, index) => (
           <motion.span
